f7861e302a03e09899068f34f7cd31f70b9579ea
zch <zchagra.dev@gmail.com>
2026-03-12 09:25:38 +0100

    ENV : add blank env file

---
+++ src/app/env.ts
@@ -0,0 +1,13 @@
+export const env = {
+  geminiApiKey:'XXXXX XXXXX',
+  firebase: {
+    apiKey: "XXXXX XXXXX",
+  authDomain: "XXXXX XXXXX",
+  projectId: "XXXXX XXXXX",
+  storageBucket: "XXXXX XXXXX",
+  messagingSenderId: "XXXXX XXXXX",
+  appId: "XXXXX XXXXX",
+  measurementId: "XXXXX XXXXX"
+  },
+};
+// IMPORTANT please update the env.ts file with your own values.
